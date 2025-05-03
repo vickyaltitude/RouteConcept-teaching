5075813c2b1e5d7a6d5f375f76f2650772ac5003
vickyaltitude <vignvick3005>
2025-05-03 15:29:43 +0530

    completed axios

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 const About = () => {
+  useEffect(() => {
+    async function getData() {
+      const getData = await axios("https://jsonplaceholder.typicode.com/users");
+      console.log(getData.data);
+    }
+    getData();
+  }, []);
+
   return <div>About</div>;
 };
 
